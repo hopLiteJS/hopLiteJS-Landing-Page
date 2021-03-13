d0bb742d63ee0a0c674f072d7fe0163542af595d
CodeWithYourHeart <65440123+CodeWithYourHeart@users.noreply.github.com>
2021-03-13 13:59:03 -0800

    added navbar and react bootsrap library

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Navbar from 'react-bootstrap/Navbar'
+import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>HopLiteJS</title>
-        <link rel="icon" href="/favicon.ico" />
+       
 
-
-
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">HopLite-JS</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Documentation</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
         
       </Head>
 
