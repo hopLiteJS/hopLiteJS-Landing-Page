1ed4a91ffd2b4a02c699e44cb076d06a115b7bf5
karin illesova <karin.illesova@gmail.com>
2021-03-24 04:49:36 +0100

    refactoring started

--- components/LandingPage/ContactUs.js
+++ components/LandingPage/ContactUs.js
@@ -1,4 +1,33 @@
 import React from "react";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+  paper: {
+    backgroundColor: 'pink'
+  }
+}));
+
 export default function ContactUs() {
-  return <div className = "ContactUs"><h1>ContactUs</h1><h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h2></div>
+  const classes = useStyles();
+  return (
+    <div className = "ContactUs">
+      <h1>ContactUs</h1>
+      <Paper className={classes.paper} elevation={6}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </Paper>
+    </div>
+  )
 }
--- components/LandingPage/ExampleCode.js
+++ components/LandingPage/ExampleCode.js
@@ -1,9 +1,9 @@
 import React from "react";
 export default function ExampleCode() {
-  return <div className = "ExampleCode"><h1>Example Code</h1> <img
-  src="/exampleCode.png"
-  alt="Picture of the code"
-  width={500}
-  height={400}
-/></div>
+  return (
+    <div className = "ExampleCode">
+      <h1>Example Code</h1> 
+      <img src="/exampleCode.png" alt="The code" width={500} height={400}/>
+    </div>
+  )
 }
